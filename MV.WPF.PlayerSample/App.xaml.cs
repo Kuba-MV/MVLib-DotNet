fdfa5a2497b0563f8004a718c4175a68dd74b060
Kuba Głuszkiewicz <jakub.gluszkiewicz@nanovotech.pl>
2020-09-14 11:41:14 +0200

    initial commit

---
+++ MV.WPF.PlayerSample/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+MIT License
+
+Media Vault Library DotNet 
+
+Copyright (C) 2020 Jakub Gluszkiewicz (kubabrt@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+using System.Windows;
+
+using MV.DotNet.Common;
+
+namespace MV.WPF.PlayerSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //This is a good place to initialize Media Vault Library
+            MV_Manager.InitializeMediaVault();
+
+            MV_Manager.InitializeLog("mvlog.log");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            //Close media engine
+            MV_Manager.CloseMediaVault();
+            MV_Manager.DisposeLog();
+
+            base.OnExit(e);
+        }
+    }
+}
